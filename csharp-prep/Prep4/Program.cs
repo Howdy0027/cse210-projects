33743bfff9f9b5b8f1ab2facf30c89f98de858c1
Howdy0027 <tay22003@byui.edu>
2024-04-26 22:48:15 -0600

    C# Prep 4 and C# Prep 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine();                                                                                        //Spacing
+        Console.Write("C# Prep 4");                                                                                 //Writes out Title
+        Console.WriteLine();                                                                                        //Spacing
+        Console.WriteLine();                                                                                        //Spacing
+
+        Console.Write("Enter a list of numbers, type 0 when finished.");                                            //Writes out instructions
+        Console.WriteLine();                                                                                        //Spacing
+
+        List<int> numbers = new List<int>();                                                                        //Setting list to be a new (blank) list
+        
+        int userNumber = -1;                                                                                        //Setting userNumber to be -1
+        while (userNumber != 0)                                                                                     //While loop for creating asking for user input of numbers
+        {
+            Console.Write("Enter number: ");                                                                        //Ask for user input for number to add to list
+            
+            string userResponse = Console.ReadLine();                                                               //Convert number to string
+            userNumber = int.Parse(userResponse);                                                                   //Convert number to integer
+            
+            if (userNumber != 0)                                                                                    //If user input equals 0, add numbers together
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)                                                                             //For loop for creating sum number
+        {
+            sum += number;                                                                                          //Adds sum to new number
+        }
+
+        Console.WriteLine();                                                                                        //Spacing
+        Console.WriteLine($"The sum is: {sum}");                                                                    //Returns a string with sum number
+
+        float averageNumber = ((float)sum) / numbers.Count;                                                         //Converts string to float number
+        Console.WriteLine($"The average number is: {averageNumber}");                                               //Returns a string with average number
+
+        int maxNumber = numbers[0];
+
+        foreach (int number in numbers)                                                                             //For loop for searching list for max number
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;                                                                                 //If number is max then rename
+            }
+        }
+
+        Console.WriteLine($"The max is: {maxNumber}");                                                              //Returns a string with max number
+        Console.WriteLine();                                                                                        //Spacing
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    
+    static void Main(string[] args)                                                                                 //Main function
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();                                                                                    //Run first function
+
+        string userName = PromptUserName();                                                                         //Run second function
+        int userNumber = PromptUserNumber();                                                                        //Run third function
+
+        int squaredNumber = SquareNumber(userNumber);                                                               //Run fourth function
+
+        DisplayResult(userName, squaredNumber);                                                                     //Run fifth function
+    }
+
+    static void DisplayWelcomeMessage()                                                                             //First function (Title and welcome message)
+    {
+        Console.WriteLine();                                                                                        //Spacing
+        Console.Write("C# Prep 5");                                                                                 //Writes out Title
+        Console.WriteLine();                                                                                        //Spacing
+        Console.WriteLine();                                                                                        //Spacing
+        
+        Console.WriteLine("Welcome to the program!");                                                               //Writes out welcome message
+        Console.WriteLine();                                                                                        //Spacing
+    }
+
+    static string PromptUserName()                                                                                  //Second function (user input for userName)
+    {
+        Console.Write("Please enter your name: ");                                                                  //Asks for user input for user name
+        string name = Console.ReadLine();                                                                           //Converts user name to string
+
+        return name;                                                                                                //Returns function variable for name
+    }
+
+    static int PromptUserNumber()                                                                                   //Third function (user input for userNumber)
+    {
+        Console.Write("Please enter your favorite number: ");                                                       //Asks for user input for favorite number
+        int number = int.Parse(Console.ReadLine());                                                                 //Converts user number to integer
+
+        return number;                                                                                              //Returns function variable for number
+    }
+
+    static int SquareNumber(int number)                                                                             //Fourth function (square userNumber)
+    {
+        int square = number * number;                                                                               //Math equation for squaring userNumber
+        return square;                                                                                              //Returns function variable for square number
+    }
+
+    static void DisplayResult(string name, int square)                                                              //Fifth function (response with inputs)
+    {
+        Console.WriteLine();                                                                                        //Spacing
+        Console.WriteLine($"{name}, the square of your number is {square}");                                        //Returns response with inputs
+        Console.WriteLine();                                                                                        //Spacing
     }
 }
