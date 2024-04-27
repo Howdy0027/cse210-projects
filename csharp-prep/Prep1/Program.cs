638cf6c6e6debfeb0ef340d579d5825e1784f609
Howdy0027 <tay22003@byui.edu>
2024-04-26 19:57:18 -0600

    C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine();                                                                //Spacing
+        Console.Write("C# Prep 1");                                                         //Writes out Title
+        Console.WriteLine();                                                                //Spacing
+        Console.WriteLine();                                                                //Spacing
+
+        Console.Write("What is your first name? ");                                         //Input for first name
+        string fname = Console.ReadLine();                                                  //Returns a string that comes from users input
+
+        Console.Write("What is your last name? ");                                          //Input for last name
+        string lname = Console.ReadLine();                                                  //Returns a string that comes from users input
+
+        Console.WriteLine();                                                                //Spacing
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");                       //Returns a string with users input
+        Console.WriteLine();                                                                //Spacing
     }
 }
